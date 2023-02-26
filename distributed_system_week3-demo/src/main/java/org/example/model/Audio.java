13cc5f1262eb8b53b9be24b7aed37307c6671551
pranav-k-jha <61943097+pranav-k-jha@users.noreply.github.com>
2023-02-26 15:22:56 -0500

    Update Audio.java
    commit 7

--- distributed_system_week3-demo/src/main/java/org/example/model/Audio.java
+++ distributed_system_week3-demo/src/main/java/org/example/model/Audio.java
@@ -1,62 +1,91 @@
 package org.example.model;
 
+import java.util.function.Predicate;
+
 public class Audio {
+	private String id;
     private String artistName;
     private String trackTitle;
-    private String albumTitle;
+	private String albumTitle;
     private int trackNumber;
     private int year;
     private int numReviews;
     private int numCopiesSold;
+    
+  
 
-    // Constructor
-    public Audio(String artistName, String trackTitle, String albumTitle, int trackNumber, int year, int numReviews, int numCopiesSold) {
-        this.artistName = artistName;
-        this.trackTitle = trackTitle;
-        this.albumTitle = albumTitle;
-        this.trackNumber = trackNumber;
-        this.year = year;
-        this.numReviews = numReviews;
-        this.numCopiesSold = numCopiesSold;
-    }
+	public String getId() {
+		return id;
+	}
 
-    // GET methods
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
     public String getArtistName() {
         return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
     }
 
     public String getTrackTitle() {
         return trackTitle;
     }
 
+    public void setTrackTitle(String trackTitle) {
+        this.trackTitle = trackTitle;
+    }
+
     public String getAlbumTitle() {
         return albumTitle;
+    }
+
+    public void setAlbumTitle(String albumTitle) {
+        this.albumTitle = albumTitle;
     }
 
     public int getTrackNumber() {
         return trackNumber;
     }
 
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public int getNumReviews() {
         return numReviews;
     }
 
+    public void setNumReviews(int numReviews) {
+        this.numReviews = numReviews;
+    }
+
     public int getNumCopiesSold() {
         return numCopiesSold;
     }
 
-    // POST method
-    public void createAudioItem(String artistName, String trackTitle, String albumTitle, int trackNumber, int year, int numReviews, int numCopiesSold) {
-        this.artistName = artistName;
-        this.trackTitle = trackTitle;
-        this.albumTitle = albumTitle;
-        this.trackNumber = trackNumber;
-        this.year = year;
-        this.numReviews = numReviews;
+    public void setNumCopiesSold(int numCopiesSold) {
         this.numCopiesSold = numCopiesSold;
     }
+
+	public void setDuration(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Predicate<Audio> toPredicate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
